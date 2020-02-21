304b32afc770d0d20604166121622008bee2a957
zhenhua mao <zhenhua.mao@ranplanwireless.com>
2020-02-21 15:34:31 +0800

    Add project files.

---
+++ OpenGLChecker/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OpenGLChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInfo.Location = new System.Drawing.Point(0, 0);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(800, 450);
+            this.txtInfo.TabIndex = 0;
+            this.txtInfo.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInfo);
+            this.Name = "Form1";
+            this.Text = "OpenGL Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtInfo;
+    }
+}
+
---
+++ OpenGLChecker/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenGLChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtInfo.Font = new Font(SystemFonts.DefaultFont.FontFamily, 14);
+
+            try
+            {
+                txtInfo.AppendText($"OpenGL Version: {QueryOpenGlVersion()}");
+            }
+            catch (Exception ex)
+            {
+                txtInfo.AppendText(ex.Message);
+                txtInfo.AppendText(ex.StackTrace);
+            }
+        }
+
+        public string QueryOpenGlVersion()
+        {
+            // glGetString does not work without an OpenGL context.
+            using (var openGlControl = new GLControl())
+            {
+                openGlControl.MakeCurrent();
+                return GL.GetString(StringName.Version);
+            }
+        }
+    }
+}
